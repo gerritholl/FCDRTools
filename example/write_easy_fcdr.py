a8ff8e2e4a6774eeb635b214ed218fa8229e77cc
tom <tom.block@brockmann-consult.de>
2017-01-25 14:41:16 +0100

    updated comments

--- example/write_easy_fcdr.py
+++ example/write_easy_fcdr.py
@@ -8,7 +8,7 @@ def main():
     # The scan-width is set automatically
     dataset = writer.createTemplateEasy("AVHRR", 12835)
 
-    # set some mandatory global attributes. Writing will fail if not all are filled
+    # set some mandatory global attributes. Writing will fail if not all of them are filled
     dataset.attrs["institution"] = "Brockmann Consult GmbH"
     dataset.attrs["title"] = "His Majesty!"
     dataset.attrs["source"] = "fake data"
@@ -16,11 +16,11 @@ def main():
     dataset.attrs["references"] = "CDF_FCDR_File Spec"
     dataset.attrs["comment"] = "just to show ho things are intended to be used"
 
-    # write real data to the variables. They are filled with the "_FillValue".
+    # write real data to the variables. All variables initially contain "_FillValue".
     # Not writing to the whole array is completely OK
     dataset.variables["Ch1_Bt"].data[23, 44] = 0.456
 
-    # dump it to disk
+    # dump it to disk, netcdf4, medium compression
     # writing will fail when the target file already exists
     writer.write(dataset, "D:\\Satellite\\DELETE\\avhrr_fcdr_easy.nc")
 

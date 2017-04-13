d6016082196b7da8e26670c1ffcbe971e6798c22
tom <tom.block@brockmann-consult.de>
2017-04-13 10:58:18 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(name='fcdr_tools',
+      version='1.0.5',
+      description='FIDUCEO CDR/FCDR read and write utilities',
+      author='Tom Block',
+      author_email='tom.block@brockmann-consult.de',
+      url='http://www.fiduceo.eu',
+      packages=find_packages(),
+      install_requires=['numpy', 'xarray', 'netcdf4'])

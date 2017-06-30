97975e2fe0c0fea0462bf60e3b67d5585f02fd24
tom <tom.block@brockmann-consult.de>
2017-06-30 08:55:09 +0200

    merged Gerrits changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
-from fcdr_writer import FCDRWriter
+from fiduceo.fcdr.writer.fcdr_writer import FCDRWriter
 
 setup(name='fcdr_tools', version=FCDRWriter._version, description='FIDUCEO CDR/FCDR read and write utilities', author='Tom Block', author_email='tom.block@brockmann-consult.de',
       url='http://www.fiduceo.eu', packages=find_packages(), install_requires=['numpy', 'xarray', 'netcdf4'])

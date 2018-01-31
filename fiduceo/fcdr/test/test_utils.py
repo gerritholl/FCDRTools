defea2b39174492dba1bf7fc5f8469abb6d34a32
sabine <sabine.embacher@brockmann-consult.de>
2018-01-31 17:52:05 +0100

    changed array iteration for array equalisation

--- fiduceo/fcdr/test/test_utils.py
+++ fiduceo/fcdr/test/test_utils.py
@@ -2,6 +2,13 @@ import numpy as np
 
 
 def assert_array_equals_with_index_error_message(test_instance, expected, actual):
+    """
+
+    :param test_instance: unittest.TestCase
+    :param expected: numpy.ndarray
+    :param actual: numpy.ndarray
+    :return:
+    """
     self = test_instance
     ex_sh = expected.shape
     ac_sh = actual.shape
@@ -11,15 +18,13 @@ def assert_array_equals_with_index_error_message(test_instance, expected, actual
     self.assertEqual(ex_sh, ac_sh, msg)
 
     ex_it = np.nditer(expected, flags=['multi_index'])
-    ac_it = np.nditer(actual, flags=['multi_index'])
     while not ex_it.finished:
-        exp = ex_it[0]
-        act = ac_it[0]
         idx = ex_it.multi_index
+        exp = expected.item(idx)
+        act = actual.item(idx)
         msg = '\nValue at index ' + str(idx) + " is not equal to expected.\n" + \
               'expected: ' + str(exp) + '\n' + \
               'actual  : ' + str(act)
         self.assertAlmostEqual(exp, act, msg=msg)
 
         ex_it.iternext()
-        ac_it.iternext()

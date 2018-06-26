887626e68fc52c1749e3df0e8a4e9560effc9df0
tom <tom.block@brockmann-consult.de>
2018-06-26 11:13:04 +0200

    finished sst CDR

--- fiduceo/cdr/test/writer/templates/sst_test.py
+++ fiduceo/cdr/test/writer/templates/sst_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 import xarray as xr
 
 from fiduceo.cdr.writer.templates.sst import SST
@@ -16,3 +17,40 @@ class SSTTest(unittest.TestCase):
 
         Assertions.assert_geolocation_variables(self, ds, 409, 12877, chunking=self.CHUNKING)
         Assertions.assert_quality_flags(self, ds, 409, 12877, chunking=self.CHUNKING)
+
+        time = ds.variables["time"]
+        self.assertEqual((12877,), time.shape)
+        self.assertEqual(-1, time.values[166])
+        self.assertEqual(4294967295, time.attrs["_FillValue"])
+        self.assertEqual("time", time.attrs["standard_name"])
+        self.assertEqual("Acquisition time in seconds since 1970-01-01 00:00:00", time.attrs["long_name"])
+        self.assertEqual("s", time.attrs["units"])
+
+        sst = ds.variables["sst"]
+        self.assertEqual((12877, 409), sst.shape)
+        self.assertTrue(np.isnan(sst.values[196, 197]))
+        self.assertTrue(np.isnan(sst.attrs["_FillValue"]))
+        self.assertEqual("sea_surface_temperature", sst.attrs["standard_name"])
+        self.assertEqual("K", sst.attrs["units"])
+        self.assertEqual("longitude latitude", sst.attrs["coordinates"])
+
+        u_ind_sst = ds.variables["u_independent_sst"]
+        self.assertEqual((12877, 409), u_ind_sst.shape)
+        self.assertTrue(np.isnan(u_ind_sst.values[198, 199]))
+        self.assertTrue(np.isnan(u_ind_sst.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of sst due to independent effects", u_ind_sst.attrs["description"])
+        self.assertEqual("longitude latitude", u_ind_sst.attrs["coordinates"])
+
+        u_str_sst = ds.variables["u_structured_sst"]
+        self.assertEqual((12877, 409), u_str_sst.shape)
+        self.assertTrue(np.isnan(u_str_sst.values[200, 201]))
+        self.assertTrue(np.isnan(u_str_sst.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of sst due to structured effects", u_str_sst.attrs["description"])
+        self.assertEqual("longitude latitude", u_str_sst.attrs["coordinates"])
+
+        u_com_sst = ds.variables["u_common_sst"]
+        self.assertEqual((12877, 409), u_com_sst.shape)
+        self.assertTrue(np.isnan(u_com_sst.values[170, 171]))
+        self.assertTrue(np.isnan(u_com_sst.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of sst due to common effects", u_com_sst.attrs["description"])
+        self.assertEqual("longitude latitude", u_com_sst.attrs["coordinates"])
--- fiduceo/cdr/writer/templates/sst.py
+++ fiduceo/cdr/writer/templates/sst.py
@@ -1,3 +1,7 @@
+import numpy as np
+from xarray import Variable
+
+from fiduceo.common.writer.default_data import DefaultData
 from fiduceo.common.writer.templates.templateutil import TemplateUtil as tu
 
 CHUNKING = (1280, 409)
@@ -6,6 +10,26 @@ CHUNKING = (1280, 409)
 class SST:
 
     @staticmethod
-    def add_variables(ds, width, height):
-        tu.add_geolocation_variables(ds, width, height, chunksizes=CHUNKING)
-        tu.add_quality_flags(ds, width, height, chunksizes=CHUNKING)
+    def add_variables(dataset, width, height):
+        tu.add_geolocation_variables(dataset, width, height, chunksizes=CHUNKING)
+        tu.add_quality_flags(dataset, width, height, chunksizes=CHUNKING)
+
+        default_array = DefaultData.create_default_vector(height, np.int32, fill_value=4294967295)
+        variable = Variable(["y"], default_array)
+        tu.add_fill_value(variable, 4294967295)
+        variable.attrs["standard_name"] = "time"
+        variable.attrs["long_name"] = "Acquisition time in seconds since 1970-01-01 00:00:00"
+        tu.add_units(variable, "s")
+        dataset["time"] = variable
+
+        default_array = DefaultData.create_default_array(width, height, np.float32, fill_value=np.NaN)
+        variable = Variable(["y", "x"], default_array)
+        tu.add_fill_value(variable, np.NaN)
+        variable.attrs["standard_name"] = "sea_surface_temperature"
+        variable.attrs["units"] = "K"
+        variable.attrs["coordinates"] = "longitude latitude"
+        dataset["sst"] = variable
+
+        dataset["u_independent_sst"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of sst due to independent effects")
+        dataset["u_structured_sst"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of sst due to structured effects")
+        dataset["u_common_sst"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of sst due to common effects")

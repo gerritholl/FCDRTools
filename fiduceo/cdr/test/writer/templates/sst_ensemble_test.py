e159bac6344e37f61a950e2812ff095fa7b767fd
tom <tom.block@brockmann-consult.de>
2018-06-27 11:36:36 +0200

    added sst-ensemble

--- fiduceo/cdr/test/writer/templates/sst_ensemble_test.py
+++ fiduceo/cdr/test/writer/templates/sst_ensemble_test.py
@@ -1,5 +1,36 @@
 import unittest
 
+import numpy as np
+import xarray as xr
+
+from fiduceo.cdr.writer.templates.sst_ensemble import SST_ENSEMBLE
+from fiduceo.common.test.assertions import Assertions
+
 
 class SSTEnsembleTest(unittest.TestCase):
-    pass
+
+    CHUNKING = (1280, 409)
+
+    def test_add_variables(self):
+        ds = xr.Dataset()
+
+        SST_ENSEMBLE.add_variables(ds, 409, 12623, 11)
+
+        Assertions.assert_geolocation_variables(self, ds, 409, 12623, chunking=self.CHUNKING)
+        Assertions.assert_quality_flags(self, ds, 409, 12623, chunking=self.CHUNKING)
+
+        time = ds.variables["time"]
+        self.assertEqual((12623,), time.shape)
+        self.assertEqual(-1, time.values[166])
+        self.assertEqual(4294967295, time.attrs["_FillValue"])
+        self.assertEqual("time", time.attrs["standard_name"])
+        self.assertEqual("Acquisition time in seconds since 1970-01-01 00:00:00", time.attrs["long_name"])
+        self.assertEqual("s", time.attrs["units"])
+
+        sst = ds.variables["sst"]
+        self.assertEqual((11, 12623, 409), sst.shape)
+        self.assertTrue(np.isnan(sst.values[6, 198, 199]))
+        self.assertTrue(np.isnan(sst.attrs["_FillValue"]))
+        self.assertEqual("sea_surface_temperature", sst.attrs["standard_name"])
+        self.assertEqual("K", sst.attrs["units"])
+        self.assertEqual("longitude latitude", sst.attrs["coordinates"])
--- fiduceo/cdr/writer/templates/sst_ensemble.py
+++ fiduceo/cdr/writer/templates/sst_ensemble.py
@@ -1,2 +1,31 @@
+import numpy as np
+from xarray import Variable
+
+from fiduceo.common.writer.default_data import DefaultData
+from fiduceo.common.writer.templates.templateutil import TemplateUtil as tu
+
+CHUNKING = (1280, 409)
+
+
 class SST_ENSEMBLE:
-    pass
+
+    @staticmethod
+    def add_variables(dataset, width, height, num_samples=10):
+        tu.add_geolocation_variables(dataset, width, height, chunksizes=CHUNKING)
+        tu.add_quality_flags(dataset, width, height, chunksizes=CHUNKING)
+
+        default_array = DefaultData.create_default_vector(height, np.int32, fill_value=4294967295)
+        variable = Variable(["y"], default_array)
+        tu.add_fill_value(variable, 4294967295)
+        variable.attrs["standard_name"] = "time"
+        variable.attrs["long_name"] = "Acquisition time in seconds since 1970-01-01 00:00:00"
+        tu.add_units(variable, "s")
+        dataset["time"] = variable
+
+        default_array = DefaultData.create_default_array_3d(width, height, num_samples, np.float32, fill_value=np.NaN)
+        variable = Variable(["samples","y", "x"], default_array)
+        tu.add_fill_value(variable, np.NaN)
+        variable.attrs["standard_name"] = "sea_surface_temperature"
+        variable.attrs["units"] = "K"
+        variable.attrs["coordinates"] = "longitude latitude"
+        dataset["sst"] = variable

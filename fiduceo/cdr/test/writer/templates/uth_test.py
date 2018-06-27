baddaf23a29bcf41fcddeb845a6466b53b0a3fd6
tom <tom.block@brockmann-consult.de>
2018-06-27 12:32:47 +0200

    implemented requested UTH changes

--- fiduceo/cdr/test/writer/templates/uth_test.py
+++ fiduceo/cdr/test/writer/templates/uth_test.py
@@ -17,44 +17,86 @@ class UTHTest(unittest.TestCase):
         Assertions.assert_gridded_geolocation_variables(self, ds, 360, 100)
         Assertions.assert_quality_flags(self, ds, 360, 100)
 
-        time_ranges = ds.variables["time_ranges"]
-        self.assertEqual((2, 100, 360), time_ranges.shape)
-        self.assertEqual(-1, time_ranges.values[0, 66, 178])
-        self.assertEqual(4294967295, time_ranges.attrs["_FillValue"])
-        self.assertEqual("Minimum and maximum seconds of day pixel contribution time", time_ranges.attrs["description"])
-        self.assertEqual("s", time_ranges.attrs["units"])
-        self.assertEqual("lon lat", time_ranges.attrs["coordinates"])
+        time_ranges_asc = ds.variables["time_ranges_ascending"]
+        self.assertEqual((2, 100, 360), time_ranges_asc.shape)
+        self.assertEqual(-1, time_ranges_asc.values[0, 66, 178])
+        self.assertEqual(4294967295, time_ranges_asc.attrs["_FillValue"])
+        self.assertEqual("Minimum and maximum seconds of day pixel contribution time, ascending nodes", time_ranges_asc.attrs["description"])
+        self.assertEqual("s", time_ranges_asc.attrs["units"])
+        self.assertEqual("lon lat", time_ranges_asc.attrs["coordinates"])
 
-        obs_count = ds.variables["observation_count"]
-        self.assertEqual((100, 360), obs_count.shape)
-        self.assertEqual(-32767, obs_count.values[67, 179])
-        self.assertEqual(-32767, obs_count.attrs["_FillValue"])
-        self.assertEqual("Number of observations contributing to pixel value", obs_count.attrs["description"])
-        self.assertEqual("lon lat", obs_count.attrs["coordinates"])
+        time_ranges_desc = ds.variables["time_ranges_descending"]
+        self.assertEqual((2, 100, 360), time_ranges_desc.shape)
+        self.assertEqual(-1, time_ranges_desc.values[1, 67, 179])
+        self.assertEqual(4294967295, time_ranges_desc.attrs["_FillValue"])
+        self.assertEqual("Minimum and maximum seconds of day pixel contribution time, descending nodes", time_ranges_desc.attrs["description"])
+        self.assertEqual("s", time_ranges_desc.attrs["units"])
+        self.assertEqual("lon lat", time_ranges_desc.attrs["coordinates"])
 
-        uth = ds.variables["uth"]
-        self.assertEqual((100, 360), uth.shape)
-        self.assertTrue(np.isnan(uth.values[97, 198]))
-        self.assertTrue(np.isnan(uth.attrs["_FillValue"]))
-        self.assertEqual("lon lat", uth.attrs["coordinates"])
+        obs_count_asc = ds.variables["observation_count_ascending"]
+        self.assertEqual((100, 360), obs_count_asc.shape)
+        self.assertEqual(-32767, obs_count_asc.values[67, 179])
+        self.assertEqual(-32767, obs_count_asc.attrs["_FillValue"])
+        self.assertEqual("Number of observations contributing to pixel value, ascending nodes", obs_count_asc.attrs["description"])
+        self.assertEqual("lon lat", obs_count_asc.attrs["coordinates"])
 
-        u_ind_uth = ds.variables["u_independent_uth"]
-        self.assertEqual((100, 360), u_ind_uth.shape)
-        self.assertTrue(np.isnan(u_ind_uth.values[98, 199]))
-        self.assertTrue(np.isnan(u_ind_uth.attrs["_FillValue"]))
-        self.assertEqual("Uncertainty of uth due to independent effects", u_ind_uth.attrs["description"])
-        self.assertEqual("lon lat", u_ind_uth.attrs["coordinates"])
+        obs_count_desc = ds.variables["observation_count_descending"]
+        self.assertEqual((100, 360), obs_count_desc.shape)
+        self.assertEqual(-32767, obs_count_desc.values[68, 180])
+        self.assertEqual(-32767, obs_count_desc.attrs["_FillValue"])
+        self.assertEqual("Number of observations contributing to pixel value, descending nodes", obs_count_desc.attrs["description"])
+        self.assertEqual("lon lat", obs_count_desc.attrs["coordinates"])
 
-        u_str_uth = ds.variables["u_structured_uth"]
-        self.assertEqual((100, 360), u_str_uth.shape)
-        self.assertTrue(np.isnan(u_str_uth.values[0, 201]))
-        self.assertTrue(np.isnan(u_str_uth.attrs["_FillValue"]))
-        self.assertEqual("Uncertainty of uth due to structured effects", u_str_uth.attrs["description"])
-        self.assertEqual("lon lat", u_str_uth.attrs["coordinates"])
+        uth_asc = ds.variables["uth_ascending"]
+        self.assertEqual((100, 360), uth_asc.shape)
+        self.assertTrue(np.isnan(uth_asc.values[97, 198]))
+        self.assertTrue(np.isnan(uth_asc.attrs["_FillValue"]))
+        self.assertEqual("lon lat", uth_asc.attrs["coordinates"])
 
-        u_com_uth = ds.variables["u_common_uth"]
-        self.assertEqual((100, 360), u_com_uth.shape)
-        self.assertTrue(np.isnan(u_com_uth.values[70, 171]))
-        self.assertTrue(np.isnan(u_com_uth.attrs["_FillValue"]))
-        self.assertEqual("Uncertainty of uth due to common effects", u_com_uth.attrs["description"])
-        self.assertEqual("lon lat", u_com_uth.attrs["coordinates"])
+        uth_desc = ds.variables["uth_descending"]
+        self.assertEqual((100, 360), uth_desc.shape)
+        self.assertTrue(np.isnan(uth_desc.values[98, 199]))
+        self.assertTrue(np.isnan(uth_desc.attrs["_FillValue"]))
+        self.assertEqual("lon lat", uth_desc.attrs["coordinates"])
+
+        u_ind_uth_asc = ds.variables["u_independent_uth_ascending"]
+        self.assertEqual((100, 360), u_ind_uth_asc.shape)
+        self.assertTrue(np.isnan(u_ind_uth_asc.values[98, 199]))
+        self.assertTrue(np.isnan(u_ind_uth_asc.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of uth due to independent effects, ascending nodes", u_ind_uth_asc.attrs["description"])
+        self.assertEqual("lon lat", u_ind_uth_asc.attrs["coordinates"])
+
+        u_ind_uth_desc = ds.variables["u_independent_uth_descending"]
+        self.assertEqual((100, 360), u_ind_uth_desc.shape)
+        self.assertTrue(np.isnan(u_ind_uth_desc.values[99, 200]))
+        self.assertTrue(np.isnan(u_ind_uth_desc.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of uth due to independent effects, descending nodes", u_ind_uth_desc.attrs["description"])
+        self.assertEqual("lon lat", u_ind_uth_desc.attrs["coordinates"])
+
+        u_str_uth_asc = ds.variables["u_structured_uth_ascending"]
+        self.assertEqual((100, 360), u_str_uth_asc.shape)
+        self.assertTrue(np.isnan(u_str_uth_asc.values[0, 201]))
+        self.assertTrue(np.isnan(u_str_uth_asc.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of uth due to structured effects, ascending nodes", u_str_uth_asc.attrs["description"])
+        self.assertEqual("lon lat", u_str_uth_asc.attrs["coordinates"])
+
+        u_str_uth_desc = ds.variables["u_structured_uth_descending"]
+        self.assertEqual((100, 360), u_str_uth_desc.shape)
+        self.assertTrue(np.isnan(u_str_uth_desc.values[1, 202]))
+        self.assertTrue(np.isnan(u_str_uth_desc.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of uth due to structured effects, descending nodes", u_str_uth_desc.attrs["description"])
+        self.assertEqual("lon lat", u_str_uth_desc.attrs["coordinates"])
+
+        u_com_uth_asc = ds.variables["u_common_uth_ascending"]
+        self.assertEqual((100, 360), u_com_uth_asc.shape)
+        self.assertTrue(np.isnan(u_com_uth_asc.values[70, 171]))
+        self.assertTrue(np.isnan(u_com_uth_asc.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of uth due to common effects, ascending nodes", u_com_uth_asc.attrs["description"])
+        self.assertEqual("lon lat", u_com_uth_asc.attrs["coordinates"])
+
+        u_com_uth_desc = ds.variables["u_common_uth_descending"]
+        self.assertEqual((100, 360), u_com_uth_desc.shape)
+        self.assertTrue(np.isnan(u_com_uth_desc.values[71, 172]))
+        self.assertTrue(np.isnan(u_com_uth_desc.attrs["_FillValue"]))
+        self.assertEqual("Uncertainty of uth due to common effects, descending nodes", u_com_uth_desc.attrs["description"])
+        self.assertEqual("lon lat", u_com_uth_desc.attrs["coordinates"])
--- fiduceo/cdr/writer/templates/uth.py
+++ fiduceo/cdr/writer/templates/uth.py
@@ -12,28 +12,46 @@ class UTH:
         tu.add_gridded_geolocation_variables(dataset, width, height)
         tu.add_quality_flags(dataset, width, height)
 
-        default_array = DefaultData.create_default_array_3d(width, height, 2, np.int32, fill_value=4294967295)
-        variable = Variable(["bounds", "y", "x"], default_array)
-        tu.add_fill_value(variable, 4294967295)
-        tu.add_units(variable, "s")
-        variable.attrs["description"] = "Minimum and maximum seconds of day pixel contribution time"
-        variable.attrs["coordinates"] = "lon lat"
-        dataset["time_ranges"] = variable
+        dataset["time_ranges_ascending"] = UTH._create_time_ranges_variable(height, width, "Minimum and maximum seconds of day pixel contribution time, ascending nodes")
+        dataset["time_ranges_descending"] = UTH._create_time_ranges_variable(height, width, "Minimum and maximum seconds of day pixel contribution time, descending nodes")
 
-        fill_value = DefaultData.get_default_fill_value(np.int16)
-        default_array = DefaultData.create_default_array(width, height, np.int16, fill_value=fill_value)
-        variable = Variable(["y", "x"], default_array)
-        tu.add_fill_value(variable, fill_value)
-        variable.attrs["description"] = "Number of observations contributing to pixel value"
-        variable.attrs["coordinates"] = "lon lat"
-        dataset["observation_count"] = variable
+        dataset["observation_count_ascending"] = UTH._create_observation_counts_variable(height, width, "Number of observations contributing to pixel value, ascending nodes")
+        dataset["observation_count_descending"] = UTH._create_observation_counts_variable(height, width, "Number of observations contributing to pixel value, descending nodes")
 
+        dataset["uth_ascending"] = UTH._create_uth_variable(height, width)
+        dataset["uth_descending"] = UTH._create_uth_variable(height, width)
+
+        dataset["u_independent_uth_ascending"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to independent effects, ascending nodes", coordinates="lon lat")
+        dataset["u_independent_uth_descending"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to independent effects, descending nodes", coordinates="lon lat")
+        dataset["u_structured_uth_ascending"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to structured effects, ascending nodes", coordinates="lon lat")
+        dataset["u_structured_uth_descending"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to structured effects, descending nodes", coordinates="lon lat")
+        dataset["u_common_uth_ascending"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to common effects, ascending nodes", coordinates="lon lat")
+        dataset["u_common_uth_descending"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to common effects, descending nodes", coordinates="lon lat")
+
+    @staticmethod
+    def _create_uth_variable(height, width):
         default_array = DefaultData.create_default_array(width, height, np.float32, fill_value=np.NaN)
         variable = Variable(["y", "x"], default_array)
         tu.add_fill_value(variable, np.NaN)
         variable.attrs["coordinates"] = "lon lat"
-        dataset["uth"] = variable
+        return variable
 
-        dataset["u_independent_uth"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to independent effects", coordinates="lon lat")
-        dataset["u_structured_uth"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to structured effects", coordinates="lon lat")
-        dataset["u_common_uth"] = tu.create_CDR_uncertainty(width, height, "Uncertainty of uth due to common effects", coordinates="lon lat")
+    @staticmethod
+    def _create_observation_counts_variable(height, width, description):
+        fill_value = DefaultData.get_default_fill_value(np.int16)
+        default_array = DefaultData.create_default_array(width, height, np.int16, fill_value=fill_value)
+        variable = Variable(["y", "x"], default_array)
+        tu.add_fill_value(variable, fill_value)
+        variable.attrs["description"] = description
+        variable.attrs["coordinates"] = "lon lat"
+        return variable
+
+    @staticmethod
+    def _create_time_ranges_variable(height, width, description):
+        default_array = DefaultData.create_default_array_3d(width, height, 2, np.int32, fill_value=4294967295)
+        variable = Variable(["bounds", "y", "x"], default_array)
+        tu.add_fill_value(variable, 4294967295)
+        tu.add_units(variable, "s")
+        variable.attrs["description"] = description
+        variable.attrs["coordinates"] = "lon lat"
+        return variable

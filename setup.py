8d00c0a653ded1bf6453669de7e2b27ad76874c9
tom <tom.block@brockmann-consult.de>
2018-06-28 08:28:30 +0200

    corrected

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 from setuptools import find_packages
 
-from fiduceo.fcdr.writer.fcdr_writer import FCDRWriter
+from fiduceo.common.version import __version__
 
-setup(name='fcdr_tools', version=FCDRWriter._version, description='FIDUCEO CDR/FCDR read and write utilities', author='Tom Block', author_email='tom.block@brockmann-consult.de',
-      url='http://www.fiduceo.eu', packages=find_packages(), install_requires=['numpy >=1.11.0', 'xarray >=0.8.2', 'netcdf4 >=1.2.4', 'numexpr >=2.6.2', 'dask >= 0.15.2'])
+setup(name='fcdr_tools', version=__version__, description='FIDUCEO CDR/FCDR read and write utilities', author='Tom Block', author_email='tom.block@brockmann-consult.de', url='http://www.fiduceo.eu',
+      packages=find_packages(), install_requires=['numpy >=1.11.0', 'xarray >=0.8.2', 'netcdf4 >=1.2.4', 'numexpr >=2.6.2', 'dask >= 0.15.2'])
